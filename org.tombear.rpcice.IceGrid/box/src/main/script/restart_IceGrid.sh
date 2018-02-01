55cae078f31beab259f9f3ddde5b89552be484c9
Ji Zhang <ji.zhang@samsung.com>
2018-02-01 20:31:26 +0800

    Seperate config.gird configuration file and make Ice Node and Ice Registry config file in each independent file.

--- org.tombear.rpcice.IceGrid/box/src/main/script/restart_IceGrid.sh
+++ org.tombear.rpcice.IceGrid/box/src/main/script/restart_IceGrid.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-cd ${project.build.directory}/${project.build.finalName}
 ICE_GRID="icegrid"
+ICE_GRID_REGISTRY="icegridregistry"
+ICE_GRID_NODE="icegridnode"
+
+cd ${project.build.directory}/${project.build.finalName}
 echo "Stop IceGridNode and IceGridAdmin service..."
 ID=`ps -ef | grep ${ICE_GRID} | awk '{print $2}'`
 kill -9 ${ID}
 
-ICE_GRID_NODE="icegridnode"
-echo "start IceGrid with a new node"
-icegridnode --Ice.Config=config.grid &
+echo "start registry for IceGrid"
+icegridregistry --Ice.Config=config.grid.registry &
 sleep 2
+
+echo "start a node in IceGrid"
+icegridnode --Ice.Config=config.grid.node1 &
+sleep 2
+
 echo "add advance application"
-icegridadmin --Ice.Config=config.grid -e "application add application_advance.xml"
+icegridadmin -u foo -p bar --Ice.Default.Locator="DemoIceGrid/Locator:tcp -h localhost -p 4061" \
+        -e "application add application_advance.xml"
 echo "add application"
-icegridadmin --Ice.Config=config.grid -e "application add application_node_1.xml"
-sleep 2
+icegridadmin -u foo -p bar --Ice.Default.Locator="DemoIceGrid/Locator:tcp -h localhost -p 4061" \
+        -e "application add application_node_1.xml"
+sleep 1
+
 echo "enter IceGrid Admin"
 icegridadmin -u foo -p bar --Ice.Default.Locator="DemoIceGrid/Locator:tcp -h localhost -p 4061"
--- org.tombear.rpcice.IceGrid/box/src/main/script/restart_IceNode.sh
+++ org.tombear.rpcice.IceGrid/box/src/main/script/restart_IceNode.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
 
 ICE_GRID="icegrid"
+ICE_GRID_NODE="icegridnode"
+
 echo "Stop IceGridNode and IceGridAdmin service..."
 ID=`ps -ef | grep ${ICE_GRID} | awk '{print $2}'`
 kill -9 ${ID}
 
-ICE_GRID_NODE="icegridnode"
-echo "start another new node in IceGrid"
-icegridnode --Ice.Config=config.grid.node &
+echo "start a node in IceGrid"
+icegridnode --Ice.Config=config.grid.node2 &
 sleep 2
+
 echo "add advance application"
-icegridadmin --Ice.Config=config.grid.node -e "application add application_advance.xml"
+icegridadmin --Ice.Config=config.grid.node2 \
+        -e "application add application_advance.xml"
 echo "add application"
-icegridadmin --Ice.Config=config.grid.node -e "application add application_node_2.xml"
-sleep 2
+icegridadmin --Ice.Config=config.grid.node2 \
+        -e "application add application_node_2.xml"
+sleep 1
+
 echo "enter IceGrid Admin"
 icegridadmin -u foo -p bar --Ice.Default.Locator="DemoIceGrid/Locator:tcp -h 109.105.4.159 -p 4061"
